c1a15cf881e9c4724789d49be1537bc4d54c6918
Joey Racz <jjracz0621@eagle.fgcu.edu>
2022-10-09 18:09:20 -0400

    Added command for rolling passage contents of a dungeon using weighted choices.

--- main.py
+++ main.py
@@ -10,25 +10,24 @@ from sqlite3 import Error
 
 
 def create_connection(db_file):
-    conn = None
+    connection = None
     try:
-        conn = sqlite3.connect(db_file)
+        connection = sqlite3.connect(db_file)
     except Error as e:
         print(e)
 
-    return conn
-
-#def clue_select(conn, priority):
+    return connection
 
 
 database = r"C:\Users\Joey PC\Documents\PY Projects\Discord-DM-Bot\Random Bot.db"
-conn = create_connection(database)
+database_connection = create_connection(database)
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
 
-bot = commands.Bot(command_prefix='$')
+intents = discord.Intents.all()
+bot = commands.Bot(command_prefix='$', intents=intents)
 
 
 @bot.event
@@ -42,12 +41,29 @@ async def on_ready():
 
 @bot.command(name='clue')
 async def clue(ctx):
-    with conn:
-        cur = conn.cursor()
-        a = random.randint(1, 104)
-        cur.execute("SELECT clue_description FROM dungeon_clue WHERE dungeon_clue_roll=?", (a,))
-        row = cur.fetchone()[0]
+    with database_connection:
+        cursor = database_connection.cursor()
+        cursor.execute("SELECT COUNT(*) from dungeon_clue")
+        roll_limit = cursor.fetchone()[0]
+        a = random.randint(1, roll_limit)
+        cursor.execute("SELECT clue_description FROM dungeon_clue WHERE dungeon_clue_roll=?", (a,))
+        row = cursor.fetchone()[0]
         await ctx.send(row)
 
+
+@bot.command(name='passage_contents')
+async def passage_contents(ctx):
+    with database_connection:
+        database_connection.row_factory = lambda cursor, row: row[0]
+        cursor = database_connection.cursor()
+        contents_rolls = cursor.execute('SELECT passage_contents_roll FROM dungeon_passage_contents').fetchall()
+        contents_weights = cursor.execute('SELECT passage_contents_weight FROM dungeon_passage_contents').fetchall()
+        random_roll = random.choices(contents_rolls, weights=contents_weights, k=1)[0]
+        database_connection.row_factory = sqlite3.Row
+        cursor = database_connection.cursor()
+        cursor.execute("SELECT passage_contents FROM dungeon_passage_contents WHERE passage_contents_roll=?",
+                       (random_roll,))
+        row = cursor.fetchone()[0]
+        await ctx.send(row)
 
 bot.run(TOKEN)
